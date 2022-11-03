d43d96be9ac88f464942452929d84e3a6b5c5b47
EliaMagris <elia.magris@libero.it>
2022-11-03 15:21:53 +0100

    aggiornamento

--- assets/main.js
+++ assets/main.js
@@ -1,5 +1,5 @@
 let arrayRand = [];
-
+let arrayUser = [];
 for (let i = 0; i < 5; i++) {
   let functionRan = randomNumber();
 
@@ -12,4 +12,27 @@ for (let i = 0; i < 5; i++) {
 // visione dei numeri nel dom
 document.getElementById('rememberNumber').innerHTML = `${arrayRand}`;
 
+function hideElement() {
+  for (let x = 0; x < 5; x++) {
+    let findNumber = parseInt(prompt('indovina i numeri king'));
+    arrayUser.push(findNumber);
+  }
+}
+
+function hideSomthing() {
+  document.getElementById('rememberNumber').style.display = 'none';
+}
+
+setTimeout(function () {
+  hideSomthing();
+  hideElement();
+}, 5000);
+
+// if(arrayRand.includes(arrayUser)){
+//     document.getElementById('Result').innerText = "HAI VINTO"
+// }else{
+//     document.getElementById('Result').innerText = "HAI PERSO"
+// }
+
 console.log(arrayRand);
+console.log(arrayUser);
